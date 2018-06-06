fd50f40843ee28ec0b82c511cdf9a683e1c6c490
MoRoses <moroses@gmail.com>
2018-06-06 15:19:11 +0300

    Updating the install script, based on @trishume's style.

--- install.sh
+++ install.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-function loadFolder() {
-        folder=$1;
-        cd $folder;
-        [ -e 'pre.sh' ] && ./pre.sh;
-        for lnk in $( ls *.symb ); do
-		flnk=`readlink -f $lnk`;
-                ln -s $flnk ~/.${lnk%%.symb};
-        done;
-        [ -e 'post.sh' ] && ./post.sh;
-        cd ..;
+# TODO: Add diretory change
+
+## Output functions modeled on @holman design
+
+function norm() {
+        printf "\r [ \033[00;34m..\033[0m ] $1\n";
 }
 
-for item in $(ls); do
-        if [ -d $item ]; then
-                loadFolder $item;
-        fi
-done
+function quest() {
+        printf "\r [ \033[00;33m??\033[0m ] $1\n";
+}
+
+function err() {
+        printf "\r\033[2K [\033[00;31mFAIL\033[0m] $1\n";
+        echo '';
+        exit;
+}
+
+function win() {
+        printf "\r\033[2K [ \033[00;32mOK\033[0m ] $1\n";
+}
+
+# Every handle with a postfix of .symb will be linked to the home diretory with a ``.'' prefix.
+# Every hanlde with a postfix of .config will be linked to the home diretory .config folder.
+
+
+norm "Test";
+quest "Yo?";
+win "Yay!";
+err "Damn :\\ ";
 
