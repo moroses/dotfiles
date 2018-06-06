6daa0dc4cb76777d31f65be40ed088d0639acc2a
MoRoses <moroses@gmail.com>
2018-06-06 17:14:57 +0300

    WIP: getting closer to the end result

--- install.sh
+++ install.sh
@@ -4,30 +4,30 @@
 
 ## Output functions modeled on @holman design
 
-function norm() {
+function notify() {
         printf "\r [ \033[00;34m..\033[0m ] $1\n";
 }
 
-function quest() {
+function ask() {
         printf "\r [ \033[00;33m??\033[0m ] $1\n";
 }
 
-function err() {
+function fail() {
         printf "\r\033[2K [\033[00;31mFAIL\033[0m] $1\n";
         echo '';
         exit;
 }
 
-function win() {
+function success() {
         printf "\r\033[2K [ \033[00;32mOK\033[0m ] $1\n";
 }
 
+function warn() {
+        printf "\r\033[2K [\033[00;36mWARN\033[0m] $1\n";
+}
 # Every handle with a postfix of .symb will be linked to the home diretory with a ``.'' prefix.
 # Every hanlde with a postfix of .config will be linked to the home diretory .config folder.
 
-
-norm "Test";
-quest "Yo?";
-win "Yay!";
-err "Damn :\\ ";
-
+for cmd in {notify,ask,success,warn,fail}; do
+        $cmd 'Yo';
+done;
