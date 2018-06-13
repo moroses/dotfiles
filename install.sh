c28c1f4366bec57d3a147869de3c461c101b0033
MoRoses <32063627+moroses@users.noreplay.github.com>
2018-06-13 13:58:52 +0300

    Various updates; install.sh WIP

--- install.sh
+++ install.sh
@@ -4,28 +4,77 @@
 
 ## Output functions modeled on @holman design
 
+# nice print function
+# parameters:
+# --sep : four characters (preferred in color)
+# --ind : indent level (0..n)
+# ... : Text, will be broken in lines for printing on screen.
+function nPrint() {
+	local sep='\033[00;34m .. \033[0m';
+	local ind=0;
+	while [ "${1::2}" = '--' ]; do
+		case "${1}" in
+			'--sep')
+				sep="${2}";
+				shift
+				;;
+			'--ind')
+				ind="${2}";
+				shift
+				;;
+			*)
+				echo 'WTF?!?';
+				exit 5
+				;;
+		esac
+		shift
+	done
+	local indt='';
+	if [ $ind -ne 0 ]; then
+		for (( i=1; i<=${ind}; i++ )); do
+#		for i in {1.."${ind}"}; do
+			indt="${indt}\t";
+		done;
+	fi
+
+	local IFS=$'\n';
+
+	for l in "${@}"; do
+		l="$l\n";
+		l=${l//};
+		while [[ $l ]]; do
+			local o="${l%%\\n*}";
+#			if [ $o = '' ]; then
+#				break;
+#			fi
+			printf "\r [${sep}] ${indt} ${o}\n";
+			l=${l#*\\n};
+		done;
+	done;
+
+	return 0;
+}
 
 function notify() {
-        printf "\r [ \033[00;34m..\033[0m ] $1\n";
-#        echo -e " [ \e[34m..\e[0m ] $1";
+	nPrint "${@}";
 }
 
 function ask() {
-        printf "\r [ \033[00;33m??\033[0m ] $1\n";
+	nPrint --sep '\033[00;33m ?? \033[0m' "${@}"
 }
 
 function fail() {
-        printf "\r\033[2K [\033[00;31mFAIL\033[0m] $1\n";
+	nPrint --sep '\033[00;31mFAIL\033[m' "${@}";
         echo '';
         exit;
 }
 
 function success() {
-        printf "\r\033[2K [ \033[00;32mOK\033[0m ] $1\n";
+	nPrint --sep '\033[00;32m OK \033[0m' "${@}";
 }
 
 function warn() {
-        printf "\r\033[2K [\033[00;36mWARN\033[0m] $1\n";
+	nPrint --sep '\033[00;36mWARN\033[0m' "${@}";
 }
 
 # Every handle with a postfix of .symb will be linked to the home directory with a ``.'' prefix.
@@ -43,7 +92,7 @@ function main() {
 		main_help
 		return 0;
 	fi
-return 0;
+	return 0;
 }
 
 function main_help() {
@@ -52,53 +101,39 @@ function main_help() {
 		echo "${oneline[@]}";
 		return ;
 	fi
-	mapfile usage <<-EOF
-		usage: ${0} [OPTIONS] COMMAND [ARGS]
-
-		Options:
-		--help : Prints this help message and exit.
-		--version : Print the ${0} version and exit.
-		--dotfiles-src DIR : sets the directory to load .files from.
-		--target DIR : set the directory to load the .files to.
-
-		Commands:
-	EOF
+	local usage="\
+usage: ${0} [OPTIONS] COMMAND [ARGS]\n\
+\n\
+Options:\n\
+--help : Prints this help message and exit.\n\
+--version : Print the ${0} version and exit.\n\
+--dotfiles-src DIR : sets the directory to load .files from.\n\
+--target DIR : set the directory to load the .files to.\n\
+\n\
+Commands:\n\
+";
 
 	for command in "${COMMANDS[@]}"; do
 		cmds+="${command}:";
 		cmds+=$("${command}_help" --one-line);
 	done;
 
-	mapfile additional <<-EOF
-		For additional help type:
-		${0} [COMMAND] --help
-		EOF
+	local additional="\
+For additional help type:\n\
+${0} [COMMAND] --help\n\
+";
 
-	prnt=("${oneline[@]}" "${usage[@]}" "${cmds[@]}" "${additional[@]}");
-
-	local IFS=$'\n';
-	for line in "${prnt[@]}"; do
-		notify $line
-	done;
-
+	notify "$oneline";
+	notify "${usage[@]}";
+	notify --ind 1 "${cmds[@]}";
+	notify "${additional[@]}"
 }
 
 #COMMANDS+=('link');
 #COMMANDS+=('delete');
 
-#notify "Starting script."
-#mapfile test <<-EOF
-#	Yo yo!!!
-#	\e[91mFurther testing\e[0m
-#	Welcome to my new and awesome install script!!!
-#EOF
-#IFS=$'\n';
-#for line in "${test[@]}"; do
-#	notify $line;
-#done;
-
 main "${@}" || exit 1;
 
-#for cmd in {notify,ask,success,warn,fail}; do
-#        $cmd "Testing"
+#for cmd in {notify,ask,warn,success,fail}; do
+#	$cmd "Line 1.\nLine 2.";
 #done;
